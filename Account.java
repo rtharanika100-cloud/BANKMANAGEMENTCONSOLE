dcfbc45699e67002285cd1d8addde89b143bee9f
rtharanika100@gmail.com <rtharanika100@gmail.com>
2026-07-31 22:15:00 +0530

    created a day1 file

---
+++ Account.java
@@ -0,0 +1,23 @@
+public class Account {
+    private int accountId;
+    private String name;
+    private double balance;
+
+    public Account(int accountId, String name, double balance) {
+        this.accountId = accountId;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}

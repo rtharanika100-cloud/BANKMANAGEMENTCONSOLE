cdfcd13bb7daaf74bf9f5e48753a1421d077ca08
rtharanika100@gmail.com <rtharanika100@gmail.com>
2026-08-01 21:36:43 +0530

    bank account has been created and deposit operation created

--- Bank.java
+++ Bank.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
 public class Bank {
+    ArrayList<Account> accounts = new ArrayList<>();
+public void createAccount(int id, String name, double balance) {
+    for (Account acc : accounts) {
+        if (acc.getAccountId() == id) {
+            System.out.println("Duplicate Account ID not allowed");
+            return;
+        }
+    }
+    Account newAcc = new Account(id, name, balance);
+    accounts.add(newAcc);
+    System.out.println("Account created successfully");
+}
+
+//deposit
+public void deposit(int id, double amount) {
+
+    for (Account acc : accounts) {
+        if (acc.getAccountId() == id) {
+
+            double newBalance = acc.getBalance() + amount;
+
+            System.out.println("Amount deposited successfully");
+            System.out.println("New Balance: " + newBalance);
+
+            return;
+        }
+    }
+
+    System.out.println("Account not found");
+}
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,30 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bank Application Started");
+
+        Bank bank = new Bank();
+        Scanner sc = new Scanner(System.in);
+
+        // Create Account
+        System.out.print("Enter ID: ");
+        int id = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Balance: ");
+        double balance = sc.nextDouble();
+
+        bank.createAccount(id, name, balance);
+
+        // Deposit
+        System.out.print("Enter deposit amount: ");
+        double amount = sc.nextDouble();
+
+        bank.deposit(id, amount);
+        sc.close();
     }
+
 }

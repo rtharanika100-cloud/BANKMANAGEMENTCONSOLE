8ca328da8e04e04d1da3c3f96b63ffdf69956626
rtharanika100@gmail.com <rtharanika100@gmail.com>
2026-08-10 00:04:12 +0530

    Add Bank class with account creation method

--- Bank.java
+++ Bank.java
@@ -1,4 +1,19 @@
 import java.util.HashMap;
+
 public class Bank {
+
     HashMap<Integer, Account> accounts = new HashMap<>();
+
+    public void createAccount(int id, String name, double balance) {
+
+        if (accounts.containsKey(id)) {
+            System.out.println("Account already exists!");
+            return;
+        }
+
+        Account acc = new Account(id, name, balance);
+        accounts.put(id, acc);
+
+        System.out.println("Account created successfully");
+    }
 }

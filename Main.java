137b4272fef388aa11048033b8364db6bab5bf4b
rtharanika100@gmail.com <rtharanika100@gmail.com>
2026-08-09 23:59:40 +0530

    Add createAccount method to create new bank accounts

--- Main.java
+++ Main.java
@@ -1,52 +1,16 @@
-import java.util.*;
+import java.util.Scanner;
 public class Main {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
         Bank bank = new Bank();
         Scanner sc = new Scanner(System.in);
-        while (true) {
-            System.out.println("\n--- BANK MENU ---");
-            System.out.println("1. Create Account");
-            System.out.println("2. Deposit");
-            System.out.println("3. Withdraw");
-            System.out.println("4. Check Balance");
-            System.out.println("5. Exit");
-            System.out.print("Enter choices: ");
-            int choice = sc.nextInt();
-            switch (choice) {
-                case 1:
-                    System.out.print("Enter ID: ");
-                    int id = sc.nextInt();
-                    sc.nextLine();
-                    System.out.print("Enter Name: ");
-                    String name = sc.nextLine();
-                    System.out.print("Enter Balance: ");
-                    double balance = sc.nextDouble();
-                    bank.createAccount(id, name, balance);
-                    break;
-                case 2:
-                    System.out.print("Enter ID: ");
-                    int did = sc.nextInt();
-                    System.out.print("Enter Amount: ");
-                    double damt = sc.nextDouble();
-                    bank.deposit(did, damt);
-                    break;
-                case 3:
-                    System.out.print("Enter ID: ");
-                    int wid = sc.nextInt();
-                    System.out.print("Enter Amount: ");
-                    double wamt = sc.nextDouble();
-                    bank.withdraw(wid, wamt);
-                    break;
-                case 4:
-                    System.out.print("Enter ID: ");
-                    int cid = sc.nextInt();
-                    bank.checkBalance(cid);
-                    break;
-                case 5:
-                    System.out.println("Thank you!");
-                    sc.close();
-                    System.exit(0);
-            }
-        }
+        System.out.print("Enter Account ID: ");
+        int id = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Enter Name: ");
+        String name = sc.nextLine();
+        System.out.print("Enter Initial Balance: ");
+        double balance = sc.nextDouble();
+        bank.createAccount(id, name, balance);
+        sc.close();
     }
 }
